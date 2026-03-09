ec85958089d1eb0ad65cda7357ef2019902ce224
ArielRocha91 <jarielrochas.91@gmail.com>
2026-03-08 20:27:48 -0400

    se configuro la DB postregess y las credenciasles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeesModule } from './employees/employees.module';
 import { UsersModule } from './users/users.module';
@@ -6,16 +7,27 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
 
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'user_practica',
-      password: 'root',
-      database: 'db_practica',
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || '5436'),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: process.env.POSTGRES_SSL === 'true',
+        extra: {
+          ssl:
+          process.env.POSTGRES_SSL === 'true' ? {
+            rejectUnauthorized: false,
+          }
+          : null,
+      },
     }),
 
     EmployeesModule,
